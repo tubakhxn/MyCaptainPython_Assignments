b695fc63411511b468201c31c5498317cf63aacd
tubakhxn <122105012+tubakhxn@users.noreply.github.com>
2023-01-13 01:36:50 +0400

    Contains code forMyCaptain
    Writing Python code to create a function called most_frequent that takes a string and prints the letters in decreasing order of frequency.

---
+++ task4.py
@@ -0,0 +1,37 @@
+def printChar(arr, Len):
+ 
+    occ = {}
+    for i in range(Len):
+         
+        if(arr[i] in occ):
+            occ[arr[i]] = occ[arr[i]] + 1
+         
+        else:
+            occ[arr[i]] = 1
+  
+    size = len(occ)
+  
+    while (size > 0):
+  
+        
+        currentMax = 0
+        arg_max = 0
+        for key, value in occ.items():
+ 
+            if (value > currentMax or (value == currentMax and key > arg_max)):
+                arg_max = key
+                currentMax = value
+  
+      
+        print(f"{arg_max} - {currentMax}")
+  
+        # Delete the maximum value
+        occ.pop(arg_max)
+        size -= 1
+ 
+Str = "Mississippi"
+Len = len(Str)
+ 
+ 
+printChar(list(Str), Len)
+ 
